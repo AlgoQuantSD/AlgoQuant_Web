68a23bc5d119fa1a9a21bf33358fb10f19147ee4
jasonmlam <jasonlam@knights.ucf.edu>
2022-11-14 17:01:18 -0500

    AQ-152 fix href error

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav class="bg-dark-gray border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
       <div class="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="https://jasonmlam.com/" class="flex items-center">
+        <a href="https://www.google.com/" class="flex items-center">
             <img src={aqLogo} class="mr-3 h-6 sm:h-9" alt="AlgoQuant Logo" />            
         </a>
         <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
@@ -15,10 +15,10 @@ const Navbar = () => {
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-black dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <a href="#" class="block py-2 pr-4 pl-3 text-green bg-green-700 rounded md:bg-transparent md:text-green md:p-0 dark:text-light-gray" aria-current="page">Home</a>
+              <a href="https://www.google.com/" class="block py-2 pr-4 pl-3 text-green bg-green-700 rounded md:bg-transparent md:text-green md:p-0 dark:text-light-gray" aria-current="page">Home</a>
             </li>
             <li>
-              <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-gray-400 md:dark:hover:text-black dark:hover:bg-gray-700 dark:hover:text-black md:dark:hover:bg-transparent">My Account</a>
+              <a href="https://www.google.com/" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-gray-400 md:dark:hover:text-black dark:hover:bg-gray-700 dark:hover:text-black md:dark:hover:bg-transparent">My Account</a>
             </li>            
           </ul>
         </div>
