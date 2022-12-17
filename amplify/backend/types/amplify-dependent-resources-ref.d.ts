b4a6b054313473396f4e97cdc13f1235f45ca1ee
Tim Yang <tyang098@gmail.com>
2022-12-16 23:21:30 -0500

    implementing login page through aws amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "algoquantwebb8233dd1": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import NavBar from './components/NavBar';
-import HomePage from './components/HomePage';
-import './App.css';
+import NavBar from "./components/NavBar";
+import HomePage from "./components/HomePage";
+import "./App.css";
+import { withAuthenticator, Button, Heading } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-function App() {
+function App({ signOut, user }) {
   return (
     <>
+      <Heading level={1}>Hello {user.username}</Heading>
+      <Button onClick={signOut}>Sign out</Button>
+      <h2>Amplify Todos</h2>
       <NavBar />
       <HomePage />
     </>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
