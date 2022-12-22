3ab981184b886d0081784c2b1a0811e8fe37cdbd
Tim Yang <tyang098@gmail.com>
2022-12-22 15:44:01 -0500

    adding logo and renaming fields

--- src/components/SignInPage.js
+++ src/components/SignInPage.js
@@ -1,10 +1,250 @@
 import React from "react";
-import { Authenticator } from "@aws-amplify/ui-react";
+import {
+  Authenticator,
+  useAuthenticator,
+  useTheme,
+  Image,
+  View,
+  Text,
+  Button,
+  Heading,
+} from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
+import AgLogo from "/Users/timyang/Desktop/AlgoQuant_Web/src/assets/images/aqLogo.png";
+
+const components = {
+  Header() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image alt="AlgoQuant Logo" src={AgLogo} />
+      </View>
+    );
+  },
+
+  Footer() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Text color={tokens.colors.neutral[80]}>
+          &copy; All Rights Reserved
+        </Text>
+      </View>
+    );
+  },
+
+  SignIn: {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Sign in to your account
+        </Heading>
+      );
+    },
+    Footer() {
+      const { toResetPassword } = useAuthenticator();
+
+      return (
+        <View textAlign="center">
+          <Button
+            fontWeight="normal"
+            onClick={toResetPassword}
+            size="small"
+            variation="link"
+          >
+            Reset Password
+          </Button>
+        </View>
+      );
+    },
+  },
+
+  SignUp: {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Create a new account
+        </Heading>
+      );
+    },
+    Footer() {
+      const { toSignIn } = useAuthenticator();
+
+      return (
+        <View textAlign="center">
+          <Button
+            fontWeight="normal"
+            onClick={toSignIn}
+            size="small"
+            variation="link"
+          >
+            Back to Sign In
+          </Button>
+        </View>
+      );
+    },
+  },
+  ConfirmSignUp: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  SetupTOTP: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  ConfirmSignIn: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  ResetPassword: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Please enter the email associated with your account.</Text>;
+    },
+  },
+  ConfirmResetPassword: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>A verification code has been sent to your email.</Text>;
+    },
+  },
+};
+
+const formFields = {
+  signIn: {
+    email: {
+      placeholder: "Enter your email",
+    },
+  },
+  signUp: {
+    email: {
+      label: "Email:",
+      placeholder: "Enter your Email:",
+      isRequired: true,
+      order: 1,
+    },
+    given_name: {
+      label: "First Name:",
+      placeholder: "Enter your First Name:",
+      isRequired: true,
+      order: 2,
+    },
+    family_name: {
+      label: "Last Name:",
+      placeholder: "Enter your Last Name:",
+      isRequired: true,
+      order: 3,
+    },
+    phone_number: {
+      label: "Phone Number:",
+      placeholder: "Enter your Phone Number:",
+      isRequired: true,
+      order: 4,
+    },
+    password: {
+      label: "Password:",
+      placeholder: "Enter your Password:",
+      isRequired: true,
+      order: 5,
+    },
+    confirm_password: {
+      label: "Confirm Password:",
+      isRequired: true,
+      order: 6,
+    },
+  },
+  resetPassword: {
+    username: {
+      placeholder: "Enter your email:",
+    },
+  },
+  confirmResetPassword: {
+    confirmation_code: {
+      placeholder: "Enter your Confirmation Code:",
+      label: "New Label",
+      isRequired: false,
+    },
+    confirm_password: {
+      placeholder: "Enter your Password Please:",
+    },
+  },
+};
 
 const SignInPage = () => {
   return (
-    <Authenticator initialState="signUp" variation="modal">
+    <Authenticator
+      formFields={formFields}
+      components={components}
+      variation="modal"
+    >
       {({ signOut, user }) => (
         <main>
           <h1>Hello {user.username}</h1>
