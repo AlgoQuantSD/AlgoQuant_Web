731d1c95281089bb4fc0b20cd9fcbb9fb6e0d651
Tim Yang <tyang098@gmail.com>
2022-12-27 23:53:58 -0500

    commenting out imports that arent currently used

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import NavBar from "./components/NavBar";
 import HomePage from "./components/HomePage";
 import SignInPage from "./components/authentication/SignInPage";
-import SignUpPage from "./components/authentication/SignUpPage";
-import ResetPwdPage from "./components/authentication/ResetPwdPage";
+// import SignUpPage from "./components/authentication/SignUpPage";
+// import ResetPwdPage from "./components/authentication/ResetPwdPage";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <ResetPwdPage />
+      <SignInPage />
       <NavBar />
       <HomePage />
     </>
