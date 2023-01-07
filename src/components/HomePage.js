d139402e2bd3dbea171fdf863dcee7d09aac057f
Tim Yang <tyang098@gmail.com>
2023-01-07 17:21:14 -0500

    accessing jwt

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,17 @@
 import React from "react";
+import { Auth } from "aws-amplify";
+import { data } from "autoprefixer";
 
 const HomePage = () => {
+  Auth.currentAuthenticatedUser()
+    .then((user) => {
+      console.log(user);
+      const jwt = user.signInUserSession.idToken.jwtToken;
+      console.log(jwt);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   return (
     <div className="w-gull h-screen bg-dark-gray">
       <h1 className="font-semibold text-5xl text-white text-center pt-32">
