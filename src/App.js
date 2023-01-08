5e8b01b441628947aab9df9ada46ef9758dde338
Tim Yang <tyang098@gmail.com>
2023-01-08 01:01:14 -0500

    just messing with routing to figure out issue

--- src/App.js
+++ src/App.js
@@ -4,14 +4,32 @@ import SignInPage from "./components/authentication/SignInPage";
 // TO-DO: Imports are commented out until routing is implemented
 // import SignUpPage from "./components/authentication/SignUpPage";
 // import ResetPwdPage from "./components/authentication/ResetPwdPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Page1 from "./components/Page1";
 
 function App() {
   return (
     <>
       <SignInPage />
-      <NavBar />
-      <HomePage />
+
+      <BrowserRouter>
+        {/* <Routes>
+          <Route exact path="/signin">
+            <SignInPage />
+          </Route>
+        </Routes> */}
+        <Routes>
+          <Route exact path="/homepage">
+            <HomePage />
+          </Route>
+        </Routes>
+        <Routes>
+          <Route exact path="/Page1">
+            <Page1 />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
