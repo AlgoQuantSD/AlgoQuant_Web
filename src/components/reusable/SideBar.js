0cf02f41ab6fd0a0b14b1235987a78a51527ee09
Jason Lam <jasolamo@gmail.com>
2023-01-10 14:19:43 -0500

    Adding styles for Sidebar

--- src/components/reusable/SideBar.js
+++ src/components/reusable/SideBar.js
@@ -1,5 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {
+  FaHome,
+  FaBrain,
+  FaClipboardList,
+  FaMoneyBill,
+  FaCog,
+} from "react-icons/fa";
 
 const Sidebar = () => {
   return (
@@ -8,44 +15,40 @@ const Sidebar = () => {
         <li>
           <p className="text-light-gray font-light">PAGES</p>
         </li>
-        <li className="py-2 mt-4 pt-6 bg-darker-gray">
-          <Link
-            to="/home"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
-          >
-            Home
+        <li>
+          <Link to="/home" className="block py-3 mt-4 pt-3 bg-darker-gray">
+            <FaHome className="text-2xl inline mr-3 mb-1 ml-2 text-light-gray" />
+            <p className="text-l inline text-light-gray">Home</p>
           </Link>
         </li>
-        <li className="py-2 mt-4 pt-6 bg-darker-gray">
+        <li>
           <Link
             to="/createInvestor"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
+            className="block py-3 mt-4 pt-3 bg-darker-gray"
           >
-            Create Investor
+            <FaBrain className="text-2xl inline mr-3 mb-1 ml-2 text-light-gray" />
+            <p className="text-l inline text-light-gray">Create Investor</p>
           </Link>
         </li>
-        <li className="py-2 mt-4 pt-6 bg-darker-gray">
+        <li>
           <Link
             to="/backtesting"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
+            className="block py-3 mt-4 pt-3 bg-darker-gray"
           >
-            Back Testing
+            <FaClipboardList className="text-2xl inline mr-3 mb-1 ml-2 text-light-gray" />
+            <p className="text-l inline text-light-gray">Back Testing</p>
           </Link>
         </li>
-        <li className="py-2 mt-4 pt-6 bg-darker-gray">
-          <Link
-            to="/history"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
-          >
-            Transaction History
+        <li>
+          <Link to="/history" className="block py-3 mt-4 pt-3 bg-darker-gray">
+            <FaMoneyBill className="text-2xl inline mr-3 mb-1 ml-2 text-light-gray" />
+            <p className="text-l inline text-light-gray">Transaction History</p>
           </Link>
         </li>
-        <li className="py-2 mt-4 pt-6 bg-darker-gray">
-          <Link
-            to="/profile"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4"
-          >
-            My Profile
+        <li>
+          <Link to="/profile" className="block py-3 mt-4 pt-3 bg-darker-gray">
+            <FaCog className="text-2xl inline mr-3 mb-1 ml-2 text-light-gray" />
+            <p className="text-l inline text-light-gray">My Profile</p>
           </Link>
         </li>
       </ul>
