95e70413fac0f59befe0d440d8922b623c618d86
ryPattillo <ryan_pattillo@knights.ucf.edu>
2023-01-10 12:29:25 -0500

    Cleand up PageRouter

--- src/PageRouter.js
+++ src/PageRouter.js
@@ -9,9 +9,14 @@ import TransactionHistoryPage from "./components/pages/TransactionHistoryPage";
 import ProfilePage from "./components/pages/ProfilePage";
 import SignInPage from "./components/pages/SignInPage";
 
+/*
+This Component is used to wrap each Route and ensure that they cannot 
+be acessed unless the user is authenticated. If the user is authenticated
+then they will be navigated to the requested page otherwise they will be sent
+to the welcome page
+*/
 function RequireAuth({ children }) {
   const { route } = useAuthenticator((context) => [context.route]);
-  console.log(route);
   return route === "authenticated" || route === "idle" ? (
     children
   ) : (
@@ -19,7 +24,12 @@ function RequireAuth({ children }) {
   );
 }
 
-function CheckLoggedIn({ children }) {
+/*
+This Component is used to protect the login page. The user should not be able 
+to acesss the login page if they are already authenticated. In the case that they 
+are authenticated then they will be directed to the home page. 
+*/
+function ProtectLogin({ children }) {
   const { route } = useAuthenticator((context) => [context.route]);
   return route === "authenticated" ? <Navigate to="/home" replace /> : children;
 }
@@ -29,7 +39,6 @@ export function PageRouter() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
-
         <Route
           path="/home"
           element={
@@ -38,7 +47,6 @@ export function PageRouter() {
             </RequireAuth>
           }
         />
-
         <Route
           path="/createinvestor"
           element={
@@ -47,7 +55,6 @@ export function PageRouter() {
             </RequireAuth>
           }
         />
-
         <Route
           path="/backtesting"
           element={
@@ -56,7 +63,6 @@ export function PageRouter() {
             </RequireAuth>
           }
         />
-
         <Route
           path="/history"
           element={
@@ -65,7 +71,6 @@ export function PageRouter() {
             </RequireAuth>
           }
         />
-
         <Route
           path="/profile"
           element={
@@ -74,13 +79,12 @@ export function PageRouter() {
             </RequireAuth>
           }
         />
-
         <Route
           path="/login"
           element={
-            <CheckLoggedIn>
+            <ProtectLogin>
               <SignInPage />
-            </CheckLoggedIn>
+            </ProtectLogin>
           }
         />
       </Routes>
