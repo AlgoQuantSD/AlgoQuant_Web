4bb62fc09d55a511c184cf1d0851d98bb8192b51
Jason Lam <jasolamo@gmail.com>
2023-01-17 12:07:21 -0500

    Removing transaction history page

--- src/components/pages/TransactionHistoryPage.js
+++ src/components/pages/TransactionHistoryPage.js
@@ -14,36 +14,6 @@ const TransactionHistoryPage = () => {
               Transaction History
             </h1>
           </div>
-          <table className="table-auto text-white mt-5">
-            <thead>
-              <tr>
-                <th className="px-4 py-2">Job Name</th>
-                <th className="px-4 py-2">Buy/Sell</th>
-                <th className="px-4 py-2">Stock Ticker</th>
-                <th className="px-4 py-2">Number of Shares</th>
-                <th className="px-4 py-2">Amount</th>
-                <th className="px-4 py-2">Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className="px-4 py-2">Job 1</td>
-                <td className="px-4 py-2">Buy</td>
-                <td className="px-4 py-2">AAPL</td>
-                <td className="px-4 py-2">100</td>
-                <td className="px-4 py-2">$5000</td>
-                <td className="px-4 py-2">Jan 1, 2020</td>
-              </tr>
-              <tr>
-                <td className="px-4 py-2">Job 2</td>
-                <td className="px-4 py-2">Sell</td>
-                <td className="px-4 py-2">GOOGL</td>
-                <td className="px-4 py-2">50</td>
-                <td className="px-4 py-2">$3000</td>
-                <td className="px-4 py-2">Feb 15, 2020</td>
-              </tr>
-            </tbody>
-          </table>
         </div>
       </div>
     </div>
