f2a9f94e927a4cc17e3654e035c1fdcdf48f27c3
Tim Yang <tyang098@gmail.com>
2023-01-19 16:22:38 -0500

    implementing new sdk changes

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -12,7 +12,7 @@ const HomePage = () => {
       <Navbar />
       <Sidebar />
       <h1 className="font-semibold text-5xl text-white text-center pt-32">
-        HomePage{" "}
+        HomePage
       </h1>
       <p className="text-white text-center">
         Welcome {user?.attributes?.given_name}
--- src/components/reusable/NavBar.js
+++ src/components/reusable/NavBar.js
@@ -1,30 +1,20 @@
-import { React } from "react";
+import { React, useState } from "react";
 import aqLogo from "../../assets/images/aq-logo.png";
 import { Link } from "react-router-dom";
 import { useAuthenticator } from "@aws-amplify/ui-react";
-import TestApi from "../api/GetUser";
-import { Auth } from "aws-amplify";
 import { AlgoQuant } from "algoquant/lib/AlgoQuant";
-
 const Navbar = () => {
   const { user, signOut } = useAuthenticator((context) => [context.user]);
-  console.log(user?.signInUserSession?.accessToken?.jwtToken);
-  console.log(user?.attributes?.sub);
-  const requestData = {
-    user_id: user?.attributes?.sub,
-  };
 
-  // const getJwtToken = async () => {
-  //   const user = await Auth.currentAuthenticatedUser();
-  //   const token = user.signInUserSession.accessToken.jwtToken;
-  //   return token;
-  // };
-  // const token = getJwtToken();
+  const [balance, setBalance] = useState(0);
 
-  const TestApiWrapper = () => {
-    TestApi(user?.signInUserSession?.accessToken?.jwtToken);
-  };
   const AlgoQuantObj = new AlgoQuant();
+  AlgoQuantObj.getUser(user?.signInUserSession?.accessToken?.jwtToken).then(
+    (resp) => {
+      console.log(resp);
+      setBalance(resp.data.buying_power);
+    }
+  );
   return (
     <nav className="fixed top-0 w-full z-50 px-4 py-2 bg-dark-gray border-gray-300 shadow">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
@@ -50,14 +40,7 @@ const Navbar = () => {
               </button>
             </li>
             <li>
-              <button
-                className="block py-2 pr-4 pl-3 text-red rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-black md:dark:hover:bg-transparent"
-                onClick={AlgoQuantObj.apiTest(
-                  user?.signInUserSession?.accessToken?.jwtToken
-                )}
-              >
-                Test API
-              </button>
+              <a>{balance}</a>
             </li>
           </ul>
         </div>
