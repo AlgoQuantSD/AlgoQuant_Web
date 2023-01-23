6aaa580ab093d4002d5b55fcec9a2ab5b610715c
Tim Yang <tyang098@gmail.com>
2023-01-23 09:39:15 -0500

    removing unused code

--- src/App.js
+++ src/App.js
@@ -3,20 +3,14 @@ import { PageRouter } from "./PageRouter";
 import { Authenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import authConfig from "./components/authentication/aws-export";
-import AlgoquantApiContext from "./api/ApiContext";
-import { AlgoQuant } from "../node_modules/algoquant/lib/AlgoQuant";
-
 import "./App.css";
 
 Amplify.configure({ ...authConfig, Analytics: { disabled: true } });
-const algoqant = new AlgoQuant();
 
 export default function App() {
   return (
-    <AlgoquantApiContext.Provider value={algoqant}>
-      <Authenticator.Provider>
-        <PageRouter />
-      </Authenticator.Provider>
-    </AlgoquantApiContext.Provider>
+    <Authenticator.Provider>
+      <PageRouter />
+    </Authenticator.Provider>
   );
 }
