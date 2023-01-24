9db183699854e0a5dc18794aec31527fb05fb109
ryPattillo <ryan_pattillo@knights.ucf.edu>
2023-01-24 15:37:46 -0500

    fixing warning for netlify

--- src/components/singular/Modals/AccountModal.js
+++ src/components/singular/Modals/AccountModal.js
@@ -1,4 +1,4 @@
-import { React, useState, useContext, useEffect } from "react";
+import { React, useState, useContext} from "react";
 import Modal from "../Modal";
 import AlgoquantApiContext from "../../../api/ApiContext";
 
