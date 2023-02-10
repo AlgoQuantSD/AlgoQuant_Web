f2d70c95031b41723b5080099af883bf591a4b3c
Jason Lam <jasolamo@gmail.com>
2023-02-10 11:03:07 -0600

    HomePage additions

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -6,32 +6,76 @@ import Sidebar from "../reusable/SideBar";
 
 const HomePage = () => {
   // Currently hardcoded but will eventually come from API
-  const [chartData, setChartData] = useState([1000, 50, 1]);
-  const [categories, setCategories] = useState(["Jan", "Feb", "Mar"]);
+  const [chartData, setChartData] = useState([
+    50, 41, 35, 51, 4, 62, 262, 91, 134,
+  ]);
+
+  const [categories, setCategories] = useState([
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+  ]);
+
+  const [selectedTabFilter, setSelectedTabFilter] = useState("investor");
+  const [selectedGraphFilter, setSelectedGraphFilter] = useState("Today");
+
+  const graphFilters = {
+    DAY: "Today",
+    FIVE: "Past 5 days",
+    MONTH: "Past month",
+    YEAR: "Past Year",
+  };
+
+  const tabFilters = {
+    INVESTOR: "investor",
+    JOB: "job",
+    history: "history",
+  };
+
+  const [stockData, setStockData] = useState([
+    {
+      symbol: "AAPL",
+      recentPrice: 150.23,
+      open: 140.0,
+      high: 162.5,
+      low: 126.5,
+      yearHigh: 167.0,
+      yearLow: 100.0,
+      percentChanged: 1.5,
+    },
+  ]);
+
+  const handleTabFilterSelection = (filter) => {
+    setSelectedTabFilter(filter);
+    // logic to update chart data based on selected filter
+  };
+
+  const handleGraphFilterSelection = (filter) => {
+    getGraphData(filter);
+    setSelectedGraphFilter(filter);
+    // logic to update chart data based on selected filter
+  };
 
   /*
     Callback used to get more data based on the filter. Each time any of the buttons 
     are clicked this will be called to get more data. This will update the chart data which 
     will then re-render the graph
     */
-  const getData = (filter) => {
-    setChartData([]);
-    setCategories([]);
-    console.log(filter);
-  };
-
-  // Defining variables for Investing tabs
-  const [selectedFilter, setSelectedFilter] = useState("investor");
-
-  const filters = {
-    INVESTOR: "investor",
-    JOB: "job",
-    history: "history",
-  };
-
-  const handleFilterSelection = (filter) => {
-    setSelectedFilter(filter);
-    // logic to update chart data based on selected filter
+  const getGraphData = (filter) => {
+    if (filter === "Past Year") {
+      setChartData([]);
+    } else {
+      setChartData(chartData.concat(chartData));
+      setCategories(categories.concat(categories));
+      setStockData(stockData);
+      console.log("Request filter: " + filter);
+    }
   };
 
   return (
@@ -40,16 +84,73 @@ const HomePage = () => {
       <div className="flex self-stretch">
         <Sidebar />
         <div className="sm:w-3/4 md:w-5/6 lg:w-7/8 p-5">
-          <div className="flex pt-10">
-            <h2 className="text-green font-bold text-4xl m-4">Your Assets</h2>
+          <div className="pt-10">
+            <h2 className="text-green font-bold text-4xl">Your Assets</h2>
           </div>
-          <Graph
-            chartData={chartData}
-            categories={categories}
-            getData={getData}
-          />
-          <div className="flex mt-24 w-full">
-            <h3 className="text-green font-bold text-4xl m-4">Invest</h3>
+          <h2 className="text-white font-semibold text-5xl mt-2">
+            ${stockData[0].recentPrice}
+          </h2>
+          <p className="text-bright-green font-medium text-md mt-2">
+            {stockData[0].recentPrice - stockData[0].open >= 0 ? "+" : "-"} $
+            {Math.abs(stockData[0].recentPrice - stockData[0].open).toFixed(2)}{" "}
+            ({stockData[0].percentChanged}
+            %)
+            <p className="inline text-light-gray font-light">
+              {" "}
+              {selectedGraphFilter}
+            </p>
+          </p>
+          <div className="w-11/12 mx-auto my-10">
+            <Graph
+              chartData={chartData}
+              categories={categories}
+              getGraphData={getGraphData}
+            />
+            <div className="flex mt-4 justify-center">
+              <button
+                className={`py-2 px-4 text-white font-semibold border-b-2 border-dark-gray hover:bg-another-gray ${
+                  selectedGraphFilter === graphFilters.DAY
+                    ? "border-b-green active"
+                    : ""
+                }`}
+                onClick={() => handleGraphFilterSelection(graphFilters.DAY)}
+              >
+                D
+              </button>
+              <button
+                className={`py-2 px-4 text-white font-semibold border-b-2 border-dark-gray hover:bg-another-gray ${
+                  selectedGraphFilter === graphFilters.FIVE
+                    ? "border-b-green active"
+                    : ""
+                }`}
+                onClick={() => handleGraphFilterSelection(graphFilters.FIVE)}
+              >
+                5D
+              </button>
+              <button
+                className={`py-2 px-4 text-white font-semibold border-b-2 border-dark-gray hover:bg-another-gray ${
+                  selectedGraphFilter === graphFilters.MONTH
+                    ? "border-b-green active"
+                    : ""
+                }`}
+                onClick={() => handleGraphFilterSelection(graphFilters.MONTH)}
+              >
+                M
+              </button>
+              <button
+                className={`py-2 px-4 text-white font-semibold border-b-2 border-dark-gray hover:bg-another-gray ${
+                  selectedGraphFilter === graphFilters.YEAR
+                    ? "border-b-green active"
+                    : ""
+                }`}
+                onClick={() => handleGraphFilterSelection(graphFilters.YEAR)}
+              >
+                Y
+              </button>
+            </div>
+          </div>
+          <div className="flex w-full">
+            <h3 className="text-green font-bold text-4xl">Invest</h3>
             <div className="pl-5 self-center">
               <Link
                 to="/createinvestor"
@@ -62,32 +163,48 @@ const HomePage = () => {
           <div className="flex mx-auto justify-center w-2/4 mt-8">
             <button
               className={`py-2 px-20 text-white border-b-2 border-dark-gray hover:bg-another-gray ${
-                selectedFilter === filters.INVESTOR
+                selectedTabFilter === tabFilters.INVESTOR
                   ? "border-b-green active"
                   : ""
               }`}
-              onClick={() => handleFilterSelection(filters.INVESTOR)}
+              onClick={() => handleTabFilterSelection(tabFilters.INVESTOR)}
             >
               Investor
             </button>
             <button
               className={`py-2 px-20 text-white border-b-2 border-dark-gray hover:bg-another-gray ${
-                selectedFilter === filters.JOB ? "border-b-green active" : ""
+                selectedTabFilter === tabFilters.JOB
+                  ? "border-b-green active"
+                  : ""
               }`}
-              onClick={() => handleFilterSelection(filters.JOB)}
+              onClick={() => handleTabFilterSelection(tabFilters.JOB)}
             >
               Job
             </button>
             <button
               className={`py-2 px-20 text-white border-b-2 border-dark-gray hover:bg-another-gray ${
-                selectedFilter === filters.history
+                selectedTabFilter === tabFilters.history
                   ? "border-b-green active"
                   : ""
               }`}
-              onClick={() => handleFilterSelection(filters.history)}
+              onClick={() => handleTabFilterSelection(tabFilters.history)}
             >
               History
             </button>
+          </div>
+          <div className="mt-10 h-screen">
+            {(() => {
+              switch (selectedTabFilter) {
+                case "investor":
+                  return <p className="text-white">Investor</p>;
+                case "job":
+                  return <p className="text-white">Job</p>;
+                case "history":
+                  return <p className="text-white">History</p>;
+                default:
+                  return null;
+              }
+            })()}
           </div>
         </div>
       </div>
