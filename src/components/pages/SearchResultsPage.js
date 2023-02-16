aa7db48ead76b6486641694962a2e9020837ae68
tim <tyang098@gmail.com>
2023-02-16 13:42:15 -0500

    padding dawg

--- src/components/pages/SearchResultsPage.js
+++ src/components/pages/SearchResultsPage.js
@@ -211,7 +211,7 @@ const SearchResultsPage = () => {
               />
             )}
 
-            <div className="flex mt-4 justify-center pb-2.5">
+            <div className="flex mt-4 justify-center pb-10">
               <button
                 className={`py-2 px-4 text-white font-semibold border-b-2 border-dark-gray hover:bg-another-gray ${
                   selectedFilter === filters.DAY ? "border-b-green active" : ""
