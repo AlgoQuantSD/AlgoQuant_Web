591ef0b4cb22add4e7952a7627373309a080f3f4
Tim Yang <tyang098@gmail.com>
2023-02-15 21:47:15 -0500

    percent change displayed

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -15,25 +15,14 @@ const HomePage = () => {
   const [categories, setCategories] = useState([1, 2, 3, 4, 5]);
 
   const [selectedTabFilter, setSelectedTabFilter] = useState("investor");
+  const [recentPrice, setRecentPrice] = useState(0);
+  const [lastIntervalChange, setLastIntervalChange] = useState(0);
 
   const tabFilters = {
     INVESTOR: "investor",
     JOB: "job",
     history: "history",
   };
-
-  const [stockData, setStockData] = useState([
-    {
-      symbol: "AAPL",
-      recentPrice: 150.23,
-      open: 140.0,
-      high: 162.5,
-      low: 126.5,
-      yearHigh: 167.0,
-      yearLow: 100.0,
-      percentChanged: 1.5,
-    },
-  ]);
 
   const handleTabFilterSelection = (filter) => {
     setSelectedTabFilter(filter);
@@ -53,6 +42,8 @@ const HomePage = () => {
           .then((resp) => {
             console.log(resp);
             setChartData(resp.data["close"]);
+            setLastIntervalChange(resp.data["interval_price_change"]);
+            setRecentPrice(resp.data["recent_price"]);
             switch (timeframe) {
               case "D":
                 setCategories(
@@ -124,9 +115,9 @@ const HomePage = () => {
             <h2 className="text-green font-bold text-4xl">Your Assets</h2>
           </div>
           <GraphStats
-            recentPrice={stockData[0].recentPrice}
-            open={stockData[0].open}
-            percentChanged={stockData[0].percentChanged}
+            recentPrice={recentPrice}
+            open={lastIntervalChange}
+            percentChanged={12}
           />
           <div className="w-11/12 mx-auto my-10 mb-32">
             <Graph
