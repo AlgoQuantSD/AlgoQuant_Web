c65869067c9fee13fd7d20f24ad3d57d59a06c09
Tim Yang <tyang098@gmail.com>
2023-02-18 09:28:07 -0500

    login screen adjustment

--- src/components/pages/SignInPage.js
+++ src/components/pages/SignInPage.js
@@ -7,12 +7,35 @@ import components from "../authentication/AuthFormComponents";
 const SignInPage = () => {
   return (
     <ThemeProvider theme={CustomAuthTheme()}>
-      <Authenticator
-        formFields={signUpConfig}
-        components={components}
-        initialState="signIn"
-        variation="modal"
-      />
+      <div className="grid grid-cols-2 gap-4 bg-green">
+        <div
+          className="bg-gray-100 p-4 text-8xl font-bold text-cokewhite font-semibold py-2 px-4 rounded-full"
+          style={{
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          Trade to your{<br />} comfort.
+        </div>
+        <div
+          className="bg-gray-200 p-4 "
+          style={{
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Authenticator
+            formFields={signUpConfig}
+            components={components}
+            initialState="signIn"
+            // variation="modal"
+          />
+        </div>
+      </div>
     </ThemeProvider>
   );
 };
