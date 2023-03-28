d1379bb856b20d2fa1d8bcacc64d325a8dd83801
tim <tyang098@gmail.com>
2023-03-28 16:34:21 -0400

    add laoding to backtest

--- src/components/pages/CreateBacktestPage.js
+++ src/components/pages/CreateBacktestPage.js
@@ -7,6 +7,7 @@ import Sidebar from "../reusable/SideBar";
 import AlgoquantApiContext from "../../api/ApiContext";
 import { useNavigate } from "react-router-dom";
 import Banner from "../reusable/Banner";
+import { BacktestSpinner } from "../reusable/LoadSpinner";
 
 const CreateBacktestPage = () => {
   const location = useLocation();
@@ -31,6 +32,8 @@ const CreateBacktestPage = () => {
 
   // State variable used to display a success message to the user
   const [success, setSuccess] = useState(false);
+  // Loading
+  const [isLoading, setIsLoading] = useState(false);
 
   // State variable used to keep track of the selected date range
   const today = new Date();
@@ -125,6 +128,7 @@ const CreateBacktestPage = () => {
     } else {
       // If all the user input is valid, display a success message
       if (algoquantApi.token) {
+        setIsLoading(true);
         algoquantApi
           .createBacktest(
             location.state.value.investor_id,
@@ -136,12 +140,13 @@ const CreateBacktestPage = () => {
           .then((resp) => {
             console.log(resp.data);
             setSuccess(true);
-            setTimeout(() => {
-              navigate("/home");
-              setSuccess(false);
-            }, 3500);
+            setIsLoading(false);
+            navigate("/home");
+            setSuccess(false);
           })
           .catch((err) => {
+            setSuccess(true);
+            setIsLoading(false);
             setErrorMsg("Error: Failed to create backtest. Try again later.");
           });
       }
@@ -158,99 +163,107 @@ const CreateBacktestPage = () => {
       <Navbar />
       <div className="flex self-stretch">
         <Sidebar />
-        <div className="sm:w-3/4 md:w-5/6 lg:w-7/8 p-5">
-          <div className="flex flex-col pt-10">
-            <h1 className="text-green font-bold text-5xl">
-              Start Backtest for {location.state.value.investor_name}
-            </h1>
+        {isLoading ? (
+          <div className="flex items-center justify-center w-full">
+            <BacktestSpinner />
           </div>
-          <div className="flex flex-col pt-3">
-            {/* Backtest Name */}
-            <h1 className="text-green font-medium text-4xl mt-10">
-              Backtest Name
-            </h1>
-            <p className="text-green text-2xl font-light m-3">
-              What would you like to call your backtest?
-            </p>
-            <input
-              className="bg-smokewhite focus:outline-none focus:shadow-outline py-2 px-4 block w-1/2 font-normal text-xl appearance-none leading-normal shadow-md caret-green text-green mb-10 ml-3"
-              type="text"
-              placeholder={"Backtest #1"}
-              onChange={(event) => {
-                setBacktestName(event.target.value);
-              }}
-            />
-            {/* Timeframe */}
-            <h1 className="text-green font-medium text-4xl">Timeframe</h1>
-            <p className="text-green text-2xl font-light m-3">
-              Select the period of time that you would like to test your
-              investor against.
-            </p>
-            <div className="flex flex-col px-24 py-12 font-semibold">
-              <Calendar
-                value={selectedDayRange}
-                maximumDate={utils().getToday()}
-                onChange={(value) => {
-                  console.log("Selected day range:", value);
-                  setSelectedDayRange(value);
-                }}
-                colorPrimary="#1F302B"
-                colorPrimaryLight="#C7C7C7"
-              />
-              <p className="text-green text-xl font-normal mt-3">
-                Selected Timeframe: {selectedDayRangeString}
+        ) : (
+          <div className="sm:w-3/4 md:w-5/6 lg:w-7/8 p-5">
+            <div className="flex flex-col pt-10">
+              <h1 className="text-green font-bold text-5xl">
+                Start Backtest for {location.state.value.investor_name}
+              </h1>
+            </div>
+            <div className="flex flex-col pt-3">
+              {/* Backtest Name */}
+              <h1 className="text-green font-medium text-4xl mt-10">
+                Backtest Name
+              </h1>
+              <p className="text-green text-2xl font-light m-3">
+                What would you like to call your backtest?
               </p>
+              <input
+                className="bg-smokewhite focus:outline-none focus:shadow-outline py-2 px-4 block w-1/2 font-normal text-xl appearance-none leading-normal shadow-md caret-green text-green mb-10 ml-3"
+                type="text"
+                placeholder={"Backtest #1"}
+                onChange={(event) => {
+                  setBacktestName(event.target.value);
+                }}
+              />
+              {/* Timeframe */}
+              <h1 className="text-green font-medium text-4xl">Timeframe</h1>
+              <p className="text-green text-2xl font-light m-3">
+                Select the period of time that you would like to test your
+                investor against.
+              </p>
+              <div className="flex flex-col px-24 py-12 font-semibold">
+                <Calendar
+                  value={selectedDayRange}
+                  maximumDate={utils().getToday()}
+                  onChange={(value) => {
+                    console.log("Selected day range:", value);
+                    setSelectedDayRange(value);
+                  }}
+                  colorPrimary="#1F302B"
+                  colorPrimaryLight="#C7C7C7"
+                />
+                <p className="text-green text-xl font-normal mt-3">
+                  Selected Timeframe: {selectedDayRangeString}
+                </p>
+              </div>
+
+              {/* Initial Investment */}
+              <h1 className="text-green font-medium text-4xl">
+                Initial Investment
+              </h1>
+              <p className="text-green text-2xl font-light m-3">
+                Provide the amount that you would like to use to test your
+                investor.
+              </p>
+              <input
+                className="bg-smokewhite focus:outline-none focus:shadow-outline py-2 px-4 block w-1/2 font-normal text-xl appearance-none leading-normal shadow-md caret-green text-green mb-10 ml-3"
+                type="text"
+                placeholder={"$0"}
+                onChange={(event) => {
+                  setInitialInvestment(event.target.value);
+                }}
+              />
             </div>
 
-            {/* Initial Investment */}
-            <h1 className="text-green font-medium text-4xl">
-              Initial Investment
-            </h1>
-            <p className="text-green text-2xl font-light m-3">
-              Provide the amount that you would like to use to test your
-              investor.
-            </p>
-            <input
-              className="bg-smokewhite focus:outline-none focus:shadow-outline py-2 px-4 block w-1/2 font-normal text-xl appearance-none leading-normal shadow-md caret-green text-green mb-10 ml-3"
-              type="text"
-              placeholder={"$0"}
-              onChange={(event) => {
-                setInitialInvestment(event.target.value);
-              }}
-            />
+            {/* Create Backtest Button */}
+            <div className="mt-10">
+              <button
+                className="text-green font-medium rounded-lg bg-cokewhite hover:bg-smokewhite border-2 border-green px-4 py-2"
+                onClick={() => {
+                  saveChanges();
+                  console.log("pressed");
+                }}
+              >
+                Start Backtest
+              </button>
+              {nameError ? (
+                <p className="text-red mt-3">
+                  Backtest name must be at least 2 characters long
+                </p>
+              ) : null}
+              {timeframeError ? (
+                <p className="text-red mt-3">
+                  Please select a timeframe for your backtest
+                </p>
+              ) : null}
+              {initialInvestmentError ? (
+                <p className="text-red mt-3">
+                  Please enter an initial investment amount
+                </p>
+              ) : null}
+              {success ? (
+                <p className="text-green mt-3">
+                  Backtest successfully created!
+                </p>
+              ) : null}
+            </div>
           </div>
-
-          {/* Create Backtest Button */}
-          <div className="mt-10">
-            <button
-              className="text-green font-medium rounded-lg bg-cokewhite hover:bg-smokewhite border-2 border-green px-4 py-2"
-              onClick={() => {
-                saveChanges();
-                console.log("pressed");
-              }}
-            >
-              Start Backtest
-            </button>
-            {nameError ? (
-              <p className="text-red mt-3">
-                Backtest name must be at least 2 characters long
-              </p>
-            ) : null}
-            {timeframeError ? (
-              <p className="text-red mt-3">
-                Please select a timeframe for your backtest
-              </p>
-            ) : null}
-            {initialInvestmentError ? (
-              <p className="text-red mt-3">
-                Please enter an initial investment amount
-              </p>
-            ) : null}
-            {success ? (
-              <p className="text-green mt-3">Backtest successfully created!</p>
-            ) : null}
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
