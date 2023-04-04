e4fa3ddb39b3dc3e639c7d8b3ed5bb9c7c8d228d
Jason Lam <jasolamo@gmail.com>
2023-04-04 19:31:36 -0400

    Adding tooltips

--- src/components/singular/IndicatorSelect.js
+++ src/components/singular/IndicatorSelect.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
+import "react-tooltip/dist/react-tooltip.css";
+import { Tooltip } from "react-tooltip";
+import { BsInfoCircleFill } from "react-icons/bs";
 
 const options = [
-  { value: "macd", label: "MACD" },
-  { value: "adl", label: "ADL" },
-  { value: "obv", label: "OBV" },
-  { value: "rsi", label: "RSI" },
-  { value: "so", label: "SO" },
-  { value: "bb", label: "BB" },
+  {
+    value: "macd",
+    label: "MACD",
+    tooltip: "Moving Average Convergence Divergence",
+  },
+  { value: "adl", label: "ADL", tooltip: "Accumulation/Distribution Line" },
+  { value: "obv", label: "OBV", tooltip: "On-Balance Volume" },
+  { value: "rsi", label: "RSI", tooltip: "Relative Strength Index" },
+  { value: "so", label: "SO", tooltip: "Stochastic Oscillator" },
+  { value: "bb", label: "BB", tooltip: "Bollinger Bands" },
 ];
 
 const IndicatorSelect = ({ onOptionsSelect }) => {
@@ -31,18 +38,31 @@ const IndicatorSelect = ({ onOptionsSelect }) => {
   return (
     <div className="p-1">
       <ul>
-        {options.map(({ value, label }) => (
-          <li key={value} className=" text-green text-lg font-bold m-3">
-            <label className="inline-flex items-center cursor-pointer">
-              <input
-                className="form-checkbox text-green-500 mr-2 h-6 w-6"
-                type="checkbox"
-                value={value}
-                checked={selectedOptions.includes(value)}
-                onChange={() => handleOptionSelect(value)}
+        {options.map(({ value, label, tooltip }) => (
+          <li key={value} className="text-green text-lg font-bold m-3">
+            <div className="flex items-center">
+              <label className="inline-flex items-center cursor-pointer">
+                <input
+                  className="form-checkbox text-green-500 mr-2 h-6 w-6"
+                  type="checkbox"
+                  value={value}
+                  checked={selectedOptions.includes(value)}
+                  onChange={() => handleOptionSelect(value)}
+                />
+                <span className="pr-2">{label}</span>
+              </label>
+              <BsInfoCircleFill
+                className="text-green text-lg my-anchor-element"
+                data-tooltip-id={`tooltip-${value}`}
+                data-tooltip-content={tooltip}
               />
-              {label}
-            </label>
+              <Tooltip
+                id={`tooltip-${value}`}
+                place="right"
+                anchorSelect=".my-anchor-element"
+                multiline={true}
+              />
+            </div>
           </li>
         ))}
       </ul>
