db1971901cbe9e31ac10b0509c1168c78b5636e6
tim <tyang098@gmail.com>
2023-04-11 21:11:30 -0400

    styling updates

--- src/components/pages/MobileLoginPage.js
+++ src/components/pages/MobileLoginPage.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const MobileLoginPage = () => {
   return (
     <div className="bg-smokewhite flex h-full w-screen justify-center items-center flex-col overflow-y-auto">
-      <h1 className="text-green font-bold text-3xl text-center mb-4">
+      <h1 className="text-green font-bold text-3xl text-center mb-4 pt-6">
         Open Algoquant
       </h1>
       <p className="text-green text-xl font-medium text-center mb-4">
@@ -21,7 +21,7 @@ const MobileLoginPage = () => {
         <img
           src={MobileApp}
           alt="mobile app"
-          className="object-contain md:object-scale-down max-h-screen"
+          className="object-contain md:object-scale-down max-h-screen pb-6 px-6"
         />
       </div>
     </div>
